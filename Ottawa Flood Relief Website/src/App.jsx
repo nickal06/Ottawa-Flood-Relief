fa39d37917b45e65cfc4203a182a857a9fa38c1d
Nicholas Alqarra <nicholas.alqarra@gmail.com>
2026-08-01 16:18:04 -0400

    Small changes: fixed routing bug and fixed up UI effects

--- Ottawa Flood Relief Website/src/App.jsx
+++ Ottawa Flood Relief Website/src/App.jsx
@@ -10,6 +10,7 @@ export function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/home" element={<Home/>} />
           <Route path="/map" element={<Map/>} />
         </Routes>
       </BrowserRouter>
